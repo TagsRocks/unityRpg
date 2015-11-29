c872e2fc525dc83b676deaf83806eee3eef8a154
liyonghelpme <liyonghelpme@gmail.com>
2015-11-29 19:41:45 +0800

    支持朝向，释放技能之前首先调整位置

---
+++ Assets/scripts/World/MapController/NetDateInterface.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace ChuMeng
+{
+    public static class NetDateInterface
+    {
+
+        public static void SyncPosAndDir()
+        {
+            var me = ObjectManager.objectManager.GetMyPlayer();
+            if (me == null)
+            {
+                return;
+            }
+            var pos = me.transform.position;
+            var dir = (int)me.transform.localRotation.eulerAngles.y;
+
+            var cg = CGPlayerCmd.CreateBuilder();
+            cg.Cmd = "UpdateData";
+            var ainfo = AvatarInfo.CreateBuilder();
+            ainfo.X = (int)(pos.x*100);
+            ainfo.Z = (int)(pos.z*100);
+            ainfo.Y = (int)(pos.y*100);
+            ainfo.Dir = dir;
+            cg.AvatarInfo = ainfo.Build();
+
+            var s = WorldManager.worldManager.GetActive();
+            s.BroadcastMsg(cg);
+        }
+    }
+}
